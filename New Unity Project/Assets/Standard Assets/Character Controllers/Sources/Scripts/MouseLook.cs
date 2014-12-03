7a71fdefe2b133624d43d9841da36d28a32b36b7
JeremyrDev <Jeremyr.dev@gmail.com>
2014-12-03 18:00:11 -0500

    Alpha 0.1.1
    Added pause and two torches, closed level for testing

--- New Unity Project/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
+++ New Unity Project/Assets/Standard Assets/Character Controllers/Sources/Scripts/MouseLook.cs
@@ -30,34 +30,66 @@ public class MouseLook : MonoBehaviour {
 
 	float rotationY = 0F;
 
-	void Update ()
-	{
-		if (axes == RotationAxes.MouseXAndY)
-		{
-			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
-			
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
-		}
-		else if (axes == RotationAxes.MouseX)
-		{
-			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
-		}
-		else
-		{
-			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
-			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
-			
-			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
-		}
-	}
+	bool paused = false;
 	
 	void Start ()
 	{
 		// Make the rigid body not change rotation
 		if (GetComponent<Rigidbody>())
 			GetComponent<Rigidbody>().freezeRotation = true;
+		Screen.lockCursor = true;
+		Screen.showCursor = false;
+		Time.timeScale = 1;
+	}
+	void Update ()
+	{
+		if(paused)
+		{
+			Screen.lockCursor = false;
+			Screen.showCursor = true;
+			Time.timeScale = 0;
+		}
+		else
+		{
+			Screen.lockCursor = true;
+			Screen.showCursor = false;
+			if (axes == RotationAxes.MouseXAndY)
+			{
+				float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+				
+				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				
+				transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+			}
+			else if (axes == RotationAxes.MouseX)
+			{
+				transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+			}
+			else
+			{
+				rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+				rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+				
+				transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
+		{
+			if(paused)
+			{
+				Screen.lockCursor = true;
+				Screen.showCursor = false;
+				paused = false;
+				Time.timeScale = 1;
+			}
+			else
+			{
+				Screen.lockCursor = false;
+				Screen.showCursor = true;
+				paused = true;
+				Time.timeScale = 0;
+			}
+		}
 	}
 }
